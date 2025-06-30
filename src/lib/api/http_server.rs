aa1b05eba8eafe16e169ffa7bc1c87fbdaf498a4
AlexRP <aleeexrp02@gmail.com>
2025-06-30 19:57:50 +0200

    Starting boilerplate for server and health endpoint

--- src/lib/api/http_server.rs
+++ src/lib/api/http_server.rs
@@ -1,12 +1,38 @@
+use anyhow::Context;
+use tokio::net;
+
+use crate::api::routes::api_routes;
+
+// State that every handlers share (used for services)
+#[derive(Debug, Clone)]
+pub struct AppState {}
+
 pub struct HttpServer {
+    router: axum::Router,
+    listener: net::TcpListener,
 }
 
 impl HttpServer {
-    pub fn new() -> Self {
-        HttpServer {}
+    pub async fn new() -> anyhow::Result<Self> {
+        let state = AppState {};
+
+        let router = axum::Router::new()
+            .nest("/api", api_routes())
+            .with_state(state);
+
+        // TODO: Change to get port from environment
+        let listener = net::TcpListener::bind("0.0.0.0:8000")
+            .await
+            .with_context(|| format!("Failed to start server on port {}", 8000))?;
+        Ok(Self { router, listener })
     }
 
-    pub fn run(&self) {
-        println!("HTTP server started");
+    /// Runs the HTTP server.
+    pub async fn run(self) -> anyhow::Result<()> {
+        println!("Listening on port {}", self.listener.local_addr().unwrap());
+        axum::serve(self.listener, self.router.into_make_service())
+            .await
+            .context("received error when running server")?;
+        Ok(())
     }
 }
---
+++ src/lib/api/routes.rs
@@ -0,0 +1,12 @@
+use axum::{response::Html, routing::get};
+
+use crate::api::http_server::AppState;
+
+pub fn api_routes() -> axum::Router<AppState> {
+    axum::Router::new()
+        .route("/health", get(health_check))
+}
+
+async fn health_check() -> Html<String> {
+    Html("<h1>API is healthy</h1>".to_string())
+}
