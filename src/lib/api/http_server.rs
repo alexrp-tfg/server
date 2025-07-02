6dc20925ae88a6ea8dbf4d2069fa379298767e5d
AlexRP <aleeexrp02@gmail.com>
2025-07-02 20:27:51 +0200

    Get port from environment

--- src/lib/api/http_server.rs
+++ src/lib/api/http_server.rs
@@ -1,5 +1,8 @@
+use std::env;
+
 use anyhow::Context;
 use tokio::net;
+use tower_http::trace::TraceLayer;
 
 use crate::api::routes::api_routes;
 
@@ -14,22 +17,28 @@ pub struct HttpServer {
 
 impl HttpServer {
     pub async fn new() -> anyhow::Result<Self> {
+        dotenvy::dotenv().context("Failed to load .env file")?;
+
         let state = AppState {};
 
         let router = axum::Router::new()
             .nest("/api", api_routes())
-            .with_state(state);
+            .with_state(state)
+        .layer(TraceLayer::new_for_http());
 
-        // TODO: Change to get port from environment
-        let listener = net::TcpListener::bind("0.0.0.0:8000")
+        let port = env::var("API_PORT").context("Failed to read API_PORT from environment")?
+            .parse::<u16>()
+            .context("Failed to parse API_PORT as u16")?;
+
+        let listener = net::TcpListener::bind(format!("0.0.0.0:{}", port))
             .await
-            .with_context(|| format!("Failed to start server on port {}", 8000))?;
+            .with_context(|| format!("Failed to start server on port {}", port))?;
         Ok(Self { router, listener })
     }
 
     /// Runs the HTTP server.
     pub async fn run(self) -> anyhow::Result<()> {
-        println!("Listening on port {}", self.listener.local_addr().unwrap());
+        println!("Listening on port {}", self.listener.local_addr().expect("Failed to get local address"));
         axum::serve(self.listener, self.router.into_make_service())
             .await
             .context("received error when running server")?;
